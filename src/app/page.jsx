30d27efb0b0ddb9ac52f10342f23a0e9a21e4b1a
sarahribeiro.tds2.senai@gmail.com <sarahribeiro.tds2.senai@gmail.com>
2025-03-11 11:01:51 -0300

    feat: create dashboardContent component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Header from '@/components/header';
 import styles from './page.module.css';
 import Sidebar from '@/components/sidebar';
+import DashboardContent from '@/components/dashboardContent';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
 
       <main className={styles.dashboardContainer}>
       < Sidebar />
+      < DashboardContent />
      </main>
     </div>
   );
